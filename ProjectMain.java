79f47bfae529b3db8558452c7fa5a66820b113f4
MatthewGabler <gablerm@sunypoly.edu>
2022-01-27 11:42:33 -0500

    very rough intro draft. What do we want to call this, anyway?

--- ProjectMain.java
+++ ProjectMain.java
@@ -1,1 +1,7 @@
+import java.util.*;
 
+public class ProjectMain {
+    public static void main(String[] args){
+        System.out.println("Welcome to CARROTODO. Enter /help for a list of commands");
+    }
+}
