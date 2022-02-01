1a3acef36c86cbbc09a1dea375bd7e903277e7aa
MatthewGabler <gablerm@sunypoly.edu>
2022-02-01 11:29:36 -0500

    Fix Intro so actually prompts for user input

--- ProjectMainIntro.java
+++ ProjectMainIntro.java
@@ -11,7 +11,7 @@ public class ProjectMainIntro {
         System.out.println("Welcome to " + ANSI_BOLD + "CARROTODO" + ANSI_RESET + ".\nEnter " + ANSI_YELLOW + "/help" + ANSI_RESET + " for a list of commands.");
         
         Scanner tokens = new Scanner(System.in);
-        String input = tokens.next();
+        String input = tokens.nextLine();
 
         if(input.equals("/help")){
 
