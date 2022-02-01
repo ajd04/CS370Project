cb9f89b41baff400ab917271b41d115c732585a7
Adam DeLucia <delucia@sunypoly.edu>
2022-01-31 19:44:17 -0500

    Added help function file

---
+++ ProjectHelp.java
@@ -0,0 +1,8 @@
+public class ProjectHelp {
+    
+    public void helpFunction(){
+
+        //Insert list of commands and a breif description of each
+
+    }
+}
--- ProjectMain.java
+++ ProjectMain.java
@@ -1,7 +1,19 @@
 import java.util.*;
 
 public class ProjectMain {
+    
     public static void main(String[] args){
+        
         System.out.println("Welcome to CARROTODO. Enter /help for a list of commands");
+
+        Scanner tokens = new Scanner(System.in);
+        String input = tokens.next();
+
+        if(input.equals("/help")){
+
+            ProjectHelp h = new ProjectHelp();
+            h.helpFunction();
+        }
     }
+
 }
