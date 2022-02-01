8e0e0351a50c9fb7938af88656b1ed9b92ea8a90
MatthewGabler <gablerm@sunypoly.edu>
2022-02-01 11:23:58 -0500

    In Intro, make CARROTODO bold and /help yellow

--- ProjectMainIntro.java
+++ ProjectMainIntro.java
@@ -4,7 +4,11 @@ public class ProjectMainIntro {
     
     public static void main(String[] args){
 
-        System.out.println("Welcome to CARROTODO.\nEnter /help for a list of commands.");
+        final String ANSI_RESET = "\u001B[0m";
+        final String ANSI_YELLOW = "\u001B[33m";
+        final String ANSI_BOLD = "\u001B[1m";
+
+        System.out.println("Welcome to " + ANSI_BOLD + "CARROTODO" + ANSI_RESET + ".\nEnter " + ANSI_YELLOW + "/help" + ANSI_RESET + " for a list of commands.");
         
         Scanner tokens = new Scanner(System.in);
         String input = tokens.next();
