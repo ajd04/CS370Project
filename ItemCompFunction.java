687374e29894e65ef0b22c5e78f314a8acb55d6d
michaeljohns175 <98195837+michaeljohns175@users.noreply.github.com>
2022-02-08 10:11:00 -0500

    Open file in ItemComp
    Added code to open the file in ItemComp.java

--- ItemCompFunction.java
+++ ItemCompFunction.java
@@ -1,6 +1,27 @@
 import java.util.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 public class ItemCompFunction extends CreateFunction {
+
+    public void ItemComp() throws IOException {
+
+    
+        //Get the file name from the user
+        System.out.println("Enter a new or existing file name that you wish to write to (w/o extension): ");
+
+        Scanner input = new Scanner(System.in);
+        String listName = input.next();
+
+        //Open the file for writing
+        BufferedWriter writer = new BufferedWriter(new FileWriter(userHomeFolder + '\\' + listName + ".txt" , true));
+
+
+        
+    }
+
 
     
     
