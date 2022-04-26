47a35d176baa61bdc872fc65f3cdebfd4770af31
Adam DeLucia <delucia@sunypoly.edu>
2022-04-26 11:04:10 -0400

    Added the new class files to the build folder

--- testing.bat
+++ testing.bat
@@ -5,6 +5,9 @@ javac -d ./build -cp ./lib/*;./build CreateExcel.java
 javac -d ./build -cp ./lib/*;./build ProjectHelp.java
 javac -d ./build -cp ./lib/*;./build ProjectIntro.java
 javac -d ./build -cp ./lib/*;./build UpdateExcel.java
+javac -d ./build -cp ./lib/*;./build ItemCompFunction.java
+javac -d ./build -cp ./lib/*;./build DeleteFunction.java
+javac -d ./build -cp ./lib/*;./build ShowListFunction.java
 
 java -cp ./lib/*;./build CarrotTODO < input.txt
 
